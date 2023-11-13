421ee35e24baf1b2b60a6997dce84a202d6753a7
Danjiil01 <danikpendo20128@gmail.com>
2023-11-13 19:34:52 +0300

    Changed the way of solution

--- src/com/teachmeskills/lesson4/primarytasks/PrimaryTask2.java
+++ src/com/teachmeskills/lesson4/primarytasks/PrimaryTask2.java
@@ -1,24 +1,37 @@
 package com.teachmeskills.lesson4.primarytasks;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class PrimaryTask2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the array`s size: ");
-        int size = sc.nextInt();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the size of array: ");
+        int size = scanner.nextInt();
         int[] array = new int[size];
+
         for (int i = 0; i < size; i++) {
             array[i] = (int) (Math.random() * 100);
         }
-        System.out.println("Array: " + Arrays.toString(array));
-        int max = Arrays.stream(array).max().getAsInt();
-        int min = Arrays.stream(array).min().getAsInt();
-        double average = Arrays.stream(array).average().getAsDouble();
-        System.out.println("Maximum value: " + max);
-        System.out.println("Minimum value: " + min);
-        System.out.println("Average value: " + average);
-    }
 
+        int max = array[0];
+        int min = array[0];
+        int sum = 0;
+
+        for (int i = 0; i < size; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+            sum += array[i];
+        }
+
+        double average = (double) sum / size;
+
+        System.out.println("Minimal number: " + max);
+        System.out.println("Maximal number: " + min);
+        System.out.println("Average: " + average);
+    }
 }
